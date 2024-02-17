b55d6c090c8f1803ac4297c0b25b0595bebd9f26
Krushit Khokhar <blackshift2023@gmail.com>
2024-02-17 12:14:15 +0530

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeOrmConfig } from './config/typeorm.config';
+import { UsersModule } from './src/modules/users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
